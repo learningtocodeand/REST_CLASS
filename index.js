8614c571b247938c13f925f747033650817b5103
learningtocodeand <145148483+learningtocodeand@users.noreply.github.com>
2024-02-03 19:56:14 +0530

    Add files via upload
    style.css meant to be in public folder
    others excluding are in views

---
+++ index.js
@@ -0,0 +1,61 @@
+const express=require("express");
+const app = express();
+const port=8080;
+const path=require("path");
+const {v4 : uuidv4}=require('uuid');
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.set("view engine","ejs");
+app.set("views" , path.join(__dirname,"views"));
+
+let posts=[
+    {
+        id:uuidv4(),
+        username: "monica",
+        content : "i am mad"
+    },
+    {
+        id:uuidv4(),
+        username: "nisha",
+        content : "i am crazy"
+    },
+    {
+        id:uuidv4(),
+        username: "lara",
+        content : "i am hehe"
+    }
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    //console.log(req.body);
+    let {username,content} = req.body;
+    let newid=uuidv4();
+    posts.push({newid,username , content});
+    //res.send("working.....");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    //console.log(id);
+    let post=posts.find((p) => id ===p.id);
+    //console.log(post);
+    res.render("show.ejs",{post});
+});
+
+
+app.listen(port,()=>{
+    console.log("listenening to port : 8080");
+});
+
